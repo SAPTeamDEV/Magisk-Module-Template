030f1d9f369fc5d42a2d83d6e810174c29c0eef0
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2022-12-28 19:09:21 +0330

    add setup file

---
+++ setup.py
@@ -0,0 +1,45 @@
+from lib2to3.pgen2.token import OP
+import os
+from turtle import fd
+
+# Change this values according to your Project.
+ModuleName = os.path.split(os.path.split(__file__)[0])[1]
+ModuleDescription = ""
+
+ScriptName = os.path.split(__file__)[1]
+
+# Check name
+if ' ' in ModuleName:
+	raise NameError("Module name have space.")
+elif ModuleName == 'Magisk-Module-Template':
+	raise NotImplementedError()
+
+def makeTree(source = '.'):
+	tree = []
+	for p in os.listdir(source):
+		if os.path.isdir(os.path.join(source, p)) and not p in ['.git', '.vs']:
+			for path in makeTree(os.path.join(source, p)):
+				tree.append(path)
+		elif os.path.isfile(os.path.join(source, p)) and not p in [ScriptName]:
+			tree.append(os.path.join(source, p))
+	return tree
+
+for f in makeTree():
+	with open(f, 'r') as file:
+		fData = file.read()
+	
+	hasModified = False
+	# Replace module name
+	if '@(ModuleName)' in fData:
+		fData = fData.replace('@(ModuleName)', ModuleName)
+		hasModified = True
+	#Replace module description
+	if '@(ModuleDescription)' in fData:
+		fData = fData.replace('@(ModuleDescription)', ModuleDescription)
+		hasModified = True
+	
+	# Do modifications
+	if hasModified:
+		with open(f 'w') as file:
+			file.write(fData)
+
