ef33760c8671a2712c2741b548dd60f017b77cce
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2023-04-03 11:30:35 +0330

    Add ModuleVersion and ModuleVersionCode properties

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ from turtle import fd
 # Change this values according to your Project.
 ModuleName = os.path.split(os.path.split(__file__)[0])[1]
 ModuleDisplayName = ModuleName.replace('-', ' ')
+ModuleVersion = "1.0"
+ModuleVersionCode = ModuleVersion.replace('.', '')
 ModuleDescription = ""
 ModuleAuthorName = ""
 
@@ -35,14 +37,25 @@ for f in makeTree():
 	if '@(ModuleName)' in fData:
 		fData = fData.replace('@(ModuleName)', ModuleName)
 		hasModified = True
+		
 	# Replace module display name
 	if '@(ModuleDisplayName)' in fData:
 		fData = fData.replace('@(ModuleDisplayName)', ModuleDisplayName)
 		hasModified = True
+		
 	#Replace module description
 	if '@(ModuleDescription)' in fData:
 		fData = fData.replace('@(ModuleDescription)', ModuleDescription)
 		hasModified = True
+		
+	# Replace module version data
+	if '@(ModuleVersion)' in fData:
+		fData = fData.replace('@(ModuleVersion)', ModuleVersion)
+		hasModified = True
+	if '@(ModuleVersionCode)' in fData:
+		fData = fData.replace('@(ModuleVersionCode)', ModuleVersionCode)
+		hasModified = True
+		
 	# Replace module author name
 	if '@(ModuleAuthorName)' in fData:
 		fData = fData.replace('@(ModuleAuthorName)', ModuleAuthorName)
